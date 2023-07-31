ae3305559e9bb688695efd5c35e878fada172a51
Joao Paulo <Jpcosta84@hotmail.com.br>
2023-07-31 11:12:39 -0300

    feat: add 'api taxa'

---
+++ ApiTaxas/Controllers/TaxasController.cs
@@ -0,0 +1,22 @@
+﻿using ApiTaxas.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTaxas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaxasController : ControllerBase
+    {
+        private readonly ITaxasService _taxasService;
+        public TaxasController(ITaxasService taxasService)
+        {
+            _taxasService = taxasService;
+        }
+        [HttpGet]
+        public IActionResult GetTaxaJuros()
+        {
+            var taxaJuros = _taxasService.GetTaxaJuros();
+            return Ok(taxaJuros);
+        }
+    }
+}
---
+++ ApiTaxas/Services/TaxasService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTaxas.Services
+{
+    public interface ITaxasService
+    {
+        decimal GetTaxaJuros();
+    }
+    public class TaxasService : ITaxasService
+    {
+        public decimal GetTaxaJuros()
+        {
+            return 0.01m;
+        }
+    }
+}
