265bcd367a2e6785b4f18a3fecd9d04b2ce90700
Joao Paulo <Jpcosta84@hotmail.com.br>
2023-07-31 11:13:39 -0300

    test: add api taxas tests

---
+++ Taxas.Test/Controllers/TaxasControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using ApiTaxas.Controllers;
+using ApiTaxas.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Taxas.Test.Controllers
+{
+    public class TaxasControllerTests
+    {
+        private readonly TaxasController _taxasController;
+        private readonly Mock<ITaxasService> _taxasServiceMock;
+
+        public TaxasControllerTests()
+        {
+            _taxasServiceMock = new Mock<ITaxasService>();
+            _taxasController = new TaxasController(_taxasServiceMock.Object);
+        }
+
+        [Fact]
+        public void GetTaxaJuros_DeveRetornarOkComTaxaJuros()
+        {
+            // Arrange
+            decimal taxaJurosEsperada = 0.01m;
+            _taxasServiceMock.Setup(x => x.GetTaxaJuros()).Returns(taxaJurosEsperada);
+
+            // Act
+            var response = _taxasController.GetTaxaJuros();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(response);
+            var taxaJurosObtida = Assert.IsType<decimal>(okResult.Value);
+            Assert.Equal(taxaJurosEsperada, taxaJurosObtida);
+        }
+    }
+}
---
+++ Taxas.Test/Services/TaxasServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using ApiTaxas.Services;
+
+namespace Taxas.Test.Services
+{
+    public class TaxasServiceTests
+    {
+        [Fact]
+        public void GetTaxaJuros_DeveRetornarTaxaJurosEsperada()
+        {
+            // Arrange
+            var taxasService = new TaxasService();
+
+            // Act
+            var taxaJuros = taxasService.GetTaxaJuros();
+
+            // Assert
+            decimal taxaJurosEsperada = 0.01m;
+            Assert.Equal(taxaJurosEsperada, taxaJuros);
+        }
+    }
+}
